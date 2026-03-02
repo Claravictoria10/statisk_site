b5262999c73a2783bf1c85186c9a348f9ca2b4c9
clara <clst1000@stud.ek.dk>
2026-03-02 12:59:53 +0100

    diverse ændringer efter gennemkig i undervisningen

--- productlist.js
+++ productlist.js
@@ -1,34 +1,48 @@
 const category = new URLSearchParams(window.location.search).get("category");
-
-console.log(category);
-("productlist.js is connected");
+console.log("category:", category);
+console.log("productlist.js is connected");
 
 const container = document.querySelector(".grid");
 
-const endpoint =
-  "https://kea-alt-del.dk/t7/api/products?limit=12&category=${category}";
+const endpoint = `https://kea-alt-del.dk/t7/api/products?limit=12&category=${category}`;
 
 function getData() {
-  fetch(endpoint).then((respons) => respons.json().then(showData));
+  fetch(endpoint)
+    .then((response) => response.json())
+    .then(showData)
+    .catch((err) => console.error("Fetch fejl:", err));
 }
 
 function showData(data) {
   console.table(data);
+
   let markup = "";
-  data.array.forEach((element) => {
-    console.log(element);
-    markup += `<article class="productlist">
-            <img
-              src="https://kea-alt-del.dk/t7/images/webp/640/${element.id}.webp"
-              alt="product image"
-            />
-            <h3>${element.productdislayname}</h3>
-            <p class="product">T-shirt</p>
-            <p class="price">DKK <span>899</span>,-</p>
-            <div class="discounted">
-              <p>Now DKK <span>699</span>.-</p>
-              <p><span>%</span></p>
-            </div>
-          </article>`;
+
+  data.forEach((element) => {
+    markup += `
+      <a href="productdetails.html?id=${element.id}">
+        <article class="productlist">
+          <img
+            src="https://kea-alt-del.dk/t7/images/webp/640/${element.id}.webp"
+            alt="${element.productdisplayname}"
+          />
+          <h3>${element.productdisplayname}</h3>
+          <p class="product">${element.articletype || ""}</p>
+          <p class="price">DKK <span>${element.price}</span>,-</p>
+
+          ${
+            element.discount
+              ? `<div class="discounted">
+                   <p>Discount: <span>${element.discount}%</span></p>
+                 </div>`
+              : ""
+          }
+        </article>
+      </a>
+    `;
   });
+
+  container.innerHTML = markup;
 }
+
+getData();
